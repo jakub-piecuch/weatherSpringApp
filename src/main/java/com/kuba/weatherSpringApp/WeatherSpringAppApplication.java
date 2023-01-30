685980b162dc8567daa13418dbe6d1ccd4705b45
Jakub Piecuch <kuba.piecuch.kp@gmail.com>
2023-01-30 21:59:34 +0100

    Test: (feature) added some files to this test brach

---
+++ src/main/java/com/kuba/weatherSpringApp/WeatherSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.kuba.weatherSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSpringAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuba/weatherSpringApp/WeatherSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuba.weatherSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
